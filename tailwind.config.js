9f0c94430b4937a8781b078ca983c0433f172a9d
gerardo itzcoatl maldonado palacios <itzcoatlb@gmail.com>
2023-02-25 18:02:19 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+ 
+  theme: {
+    extend: {
+
+      backgroundImage:{
+
+        'maple':"url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FArchivo%3AFlag_of_Canada_%2528leaf%2529.svg&psig=AOvVaw2EX50tjrODtUcCw-xR0KEW&ust=1677454591875000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLj9lLrrsf0CFQAAAAAdAAAAABAI')"
+      }
+    },
+  },
+  plugins: [],
+}
